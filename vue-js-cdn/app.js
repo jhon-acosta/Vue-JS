b732002eca717c4d3c5a3b22cb155baef7b3808b
Acosta-Jhon <jhonerick2018@gmail.com>
2021-03-13 22:51:45 -0500

    1st day

---
+++ vue-js-cdn/app.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        name: 'JHON',
+        lastname: 'ACOSTA',
+        numberOne: 15,
+        numberTwo: 25,
+        show: true,
+        clients: [
+            { name: 'Maria', payment: 25 },
+            { name: 'Pao', payment: 18 },
+            { name: 'Jhon', payment: 19 }
+        ],
+        sendHTML: '<h1>By App</h1>',
+        status: false,
+        message:'Learning Vue JS'
+    },
+    computed: {
+        greetings: (text) => {
+            alert(`Buenas ${text}`);
+            //save cache and no load again
+        },
+    },
+    methods:{
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+          }
+    }
+})
