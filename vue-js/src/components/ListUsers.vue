673ce57431b4fe9ff1086bacdf99e7e83876f37c
Acosta-Jhon <jhonerick2018@gmail.com>
2021-03-14 23:00:00 -0500

    2nd da

--- vue-js/src/components/ListUsers.vue
+++ vue-js/src/components/ListUsers.vue
@@ -2,33 +2,26 @@
   <div class="flex w-1/2 items-center justify-center p-5">
     <div>
       <h1 class="text-center">{{ title }}</h1>
-      <button v-on:click="getUser">DATA</button>
-      <table class="table-fixed">
+
+      <table class="table-fixed border-2">
         <thead>
           <tr>
-            <th class="w-1/2 ...">Title</th>
-            <th class="w-1/4 ...">Author</th>
-            <th class="w-1/4 ...">Views</th>
+            <th class="w-1/2 border-2">Nombre</th>
+            <th class="w-1/4 border-2">Apellido</th>
+            <th class="w-1/4 border-2">Apodo</th>
+            <th class="w-1/4 border-2">Edad</th>
+            <th class="w-1/4 border-2">Correo</th>
+            <th class="w-1/4 border-2">Password</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Intro to CSS</td>
-            <td>Adam</td>
-            <td>858</td>
-          </tr>
-          <tr class="bg-blue-200">
-            <td>
-              A Long and Winding Tour of the History of UI Frameworks and Tools
-              and the Impact on Design
-            </td>
-            <td>Adam</td>
-            <td>112</td>
-          </tr>
-          <tr>
-            <td>Intro to JavaScript</td>
-            <td>Chris</td>
-            <td>1,280</td>
+            <td class="border-2">Intro to CSS</td>
+            <td class="border-2">Adam</td>
+            <td class="border-2">858</td>
+            <td class="border-2">Intro to CSS</td>
+            <td class="border-2">Intro to CSS</td>
+            <td class="border-2">Intro to CSS</td>
           </tr>
         </tbody>
       </table>
@@ -40,7 +33,7 @@
 import { URL } from "../enviroment/serverUrl";
 import axios from "axios";
 export default {
-  data() {
+  data: function() {
     return {
       title: "LISTADO DE USUARIOS",
       allData: [],
@@ -52,9 +45,10 @@ export default {
         .get(`${URL}/users`)
         .then((response) => {
           this.allData = response.data.data;
+          console.log(this.allData);
         })
-        .catch((err) => {
-          console.log(err);
+        .catch((error) => {
+          console.log(error);
         });
     },
   },
